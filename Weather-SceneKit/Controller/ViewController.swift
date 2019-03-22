644dd7afa8dcc86344203c02090a3434f7b78051
klang387 <klang387@yahoo.com>
2019-03-22 12:54:28 -0400

    Repo setup

---
+++ Weather-SceneKit/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather-SceneKit
+//
+//  Created by Kevin Langelier on 3/22/19.
+//  Copyright © 2019 Kevin Langelier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
